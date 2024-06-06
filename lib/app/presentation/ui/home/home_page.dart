b2d83502f1a2efa7ac435a9db64f02187fc55f17
deadpearl <adilbayevameruert@gmail.com>
2024-06-06 19:32:46 +0500

    home page get products

--- lib/app/presentation/ui/home/home_page.dart
+++ lib/app/presentation/ui/home/home_page.dart
@@ -1,33 +1,66 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:resmart/app/domain/provider/api.dart';
 import 'package:resmart/app/domain/provider/user_provider.dart';
 import 'package:resmart/app/presentation/model/Product.dart';
+import 'package:resmart/app/presentation/ui/cart/cart_page.dart';
 import 'package:resmart/app/presentation/ui/city_selector/city_selector_page.dart';
 import 'package:resmart/app/presentation/ui/custom/constant/app_color.dart';
 import 'package:resmart/app/presentation/ui/custom/constant/app_size.dart';
 import 'package:resmart/app/presentation/ui/custom/navigation/navigation_utils.dart';
 import 'package:resmart/app/presentation/ui/custom/widget/header_text.dart';
 import 'package:resmart/app/presentation/ui/custom/widget/search_text_field.dart';
+import 'package:resmart/app/presentation/ui/orders/orders_page.dart';
 import 'package:resmart/app/presentation/ui/product/product_list_item.dart';
 import 'package:resmart/app/presentation/ui/product/product_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:resmart/app/presentation/ui/profile/profile_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../custom/navigation/motion_tab_bar.dart';
+
 
 class HomePage extends StatefulWidget {
-
-  const HomePage({super.key});
-
   @override
-  State<StatefulWidget> createState() {
-    return _HomePage();
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomePage extends State<HomePage> {
 
+class _HomePageState extends State<HomePage> {
+ late Future<List<dynamic>> _futureProducts;
   final _searchController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _futureProducts = fetchProducts();
+  }
+
+  Future<List<dynamic>> fetchProducts() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString('token');
+
+    print('token $token');
+    final response = await http.get(
+      Uri.parse('$baseUrl/products'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return Scaffold(
+    return 
+    
+    Scaffold(
       backgroundColor: AppColor.background,
       body: ScrollConfiguration(
         behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
@@ -87,62 +120,290 @@ class _HomePage extends State<HomePage> {
                 ],
               ),
             ),
-            _generatePopularProducts(size)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child:  FutureBuilder<List<dynamic>>(
+        future: _futureProducts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No products found'));
+          }
+
+           return SingleChildScrollView(
+              child: _generateProducts(context, snapshot.data!),
+            );
+        },
+      ),
+            ),
           ],
         )
       ),
+      //    bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: 0,
+      //   onTap: (int index) {
+      //     // Вызываем функцию _onItemTapped и передаем в нее индекс выбранного элемента
+      //     _onItemTapped(index);
+      //   },
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       icon: Padding(
+      //         padding:
+      //             EdgeInsets.only(top: 14.0), // Увеличиваем отступ по вертикали
+      //         child: Image(
+      //           image: AssetImage('assets/images/home.png'),
+      //           height: 24, // Задаем высоту иконки
+      //         ),
+      //       ),
+      //       label: '',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Padding(
+      //         padding: EdgeInsets.symmetric(
+      //             vertical: 12.0), // Увеличиваем отступ по вертикали
+      //         child: Image(
+      //           image: AssetImage('assets/images/orders.png'),
+      //           height: 24, // Задаем высоту иконки
+      //         ),
+      //       ),
+      //       label: 'Search',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Padding(
+      //         padding: EdgeInsets.symmetric(
+      //             vertical: 12.0), // Увеличиваем отступ по вертикали
+      //         child: Image(
+      //           image: AssetImage('assets/images/cart.png'),
+      //           height: 30, // Задаем высоту иконки
+      //         ),
+      //       ),
+      //       label: 'Notifications',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Padding(
+      //         padding: EdgeInsets.symmetric(
+      //             vertical: 12.0), // Увеличиваем отступ по вертикали
+      //         child: Image(
+      //           image: AssetImage('assets/images/profile.png'),
+      //           height: 24, // Задаем высоту иконки
+      //         ),
+      //       ),
+      //       label: 'Profile',
+      //     ),
+      //   ],
+      // ),
     );
   }
+  // void _onItemTapped(int index) {
+  //   print(index);
+  //   switch (index) {
+  //     case 0:
+  //       print('NEWS:');
+  //       Navigator.pushReplacement(
+  //           context, MaterialPageRoute(builder: (context) => HomePage()));
+  //       // Обработка нажатия на элемент "News"
+  //       // Навигация на соответствующую страницу или выполнение действия
+  //       break;
+  //     case 1:
+  //       print('FIRE:');
+  //       Navigator.pushReplacement(context,
+  //           MaterialPageRoute(builder: (context) => OrdersPage()));
+  //       // Обработка нажатия на элемент "Search"
+  //       // Навигация на соответствующую страницу или выполнение действия
+  //       break;
+  //     case 2:
+  //       print('NOTIFICATIONS:');
+  //       Navigator.pushReplacement(context,
+  //           MaterialPageRoute(builder: (context) => CartPage()));
+  //       // Обработка нажатия на элемент "Notifications"
+  //       // Навигация на соответствующую страницу или выполнение действия
+  //       break;
+  //     case 3:
+  //       print('PROFILE:');
+  //       Navigator.pushReplacement(
+  //           context, MaterialPageRoute(builder: (context) => ProfilePage()));
+  //       // Обработка нажатия на элемент "Profile"
+  //       // Навигация на соответствующую страницу или выполнение действия
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
+  }
 
-  Widget _generatePopularProducts(Size size) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(left: AppSize.horizontal, bottom: 15),
-          child: const Text(
-            "Популярные товары",
-            style: TextStyle(
-                color: AppColor.text,
-                fontSize: 14,
-                fontFamily: 'Rubik',
-                fontWeight: FontWeight.normal
-            ),
+Widget _generateProducts(BuildContext context, List<dynamic> products) {
+  Size size = MediaQuery.of(context).size;
+  var itemWidth = (size.width - 54) / 2;
+  var itemHeight = itemWidth * 1.6; // Adjust the height as needed
+
+  List<Widget> productWidgets = [];
+  for (int i = 0; i < products.length; i += 2) {
+    productWidgets.add(
+      Row(
+        children: [
+          Container(
+            width: itemWidth,
+            margin: const EdgeInsets.only(right: 7, bottom: 14),
+            child: _buildProductItemContainer(context, products[i], itemWidth, itemHeight),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: AppSize.horizontal),
-          child: _generateProducts(size),
-        )
-      ],
-    );
-  }
-
-  Widget _generateProducts(Size size) {
-    var itemWidth = (size.width - AppSize.horizontal * 2 - 14) / 2;
-    List<Widget> productWidgets = [];
-    for (int i = 0; i < 6; i += 2) {
-      productWidgets.add(
-        Row(
-          children: [
-            Container(
-              width: itemWidth,
-              margin: const EdgeInsets.only(right: 7, bottom: 14),
-              child: ProductListItem(Product(), onPressed: () {
-                NavigationUtils.push(context, ProductPage(product: Product()));
-              }),
-            ),
+          if (i + 1 < products.length)
             Container(
               width: itemWidth,
               margin: const EdgeInsets.only(left: 7, bottom: 14),
-              child: ProductListItem(Product(), onPressed: () {
-                NavigationUtils.push(context, ProductPage(product: Product()));
-              }),
-            )
-          ],
-        )
-      );
-    }
-    return Column(children: productWidgets);
+              child: _buildProductItemContainer(context, products[i + 1], itemWidth, itemHeight),
+            ),
+        ],
+      ),
+    );
+  }
+  return Column(children: productWidgets);
+}
+
+Widget _buildProductItemContainer(BuildContext context, dynamic product, double width, double height) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.3),
+          spreadRadius: 2,
+          blurRadius: 5,
+          offset: Offset(0, 3),
+        ),
+      ],
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          height: height,
+          child: _buildProductItem(context, product),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _buildProductItem(BuildContext context, dynamic product) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.network(
+          product['imageUrl'],
+          fit: BoxFit.cover,
+          height: 120, // Fixed height for the image
+          width: double.infinity, // Full width of the container
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          product['name'],
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Text(
+       "${product['price'].toStringAsFixed(2)} тг",
+          style: TextStyle(fontSize: 14, color: Colors.grey),
+        ),
+      ),
+    ],
+  );
+}
+
+
+  // Widget _buildProductItem(BuildContext context, dynamic product) {
+  //   return GestureDetector(
+  //     onTap: () {
+  //       Navigator.push(
+  //         context,
+  //         MaterialPageRoute(
+  //           builder: (context) => ProductDetailPage(product: product),
+  //         ),
+  //       );
+  //     },
+  //     child: Column(
+  //       children: [
+  //         Image.network(product['imageUrl']),
+  //         Text(product['name']),
+  //         Text(product['size'] ?? ''),
+  //         Text('${product['price']}'),
+  //       ],
+  //     ),
+  //   );
+  // }
+class ProductDetailPage extends StatelessWidget {
+  final dynamic product;
+
+  ProductDetailPage({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product['name']),
+      ),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 20.0),
+            height: 40,
+            child: Stack(
+              children: [
+                Center(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(product['name'], style: TextStyle(fontSize: 24)),
+                      Container(
+                        margin: const EdgeInsets.only(left: 5, top: 6),
+                        child: Text(
+                          product['size'] ?? '',
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.7),
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 16),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 12, left: 16.0, right: 16.0),
+            padding: const EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(product['imageUrl']),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
