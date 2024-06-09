5c3ef0b6a4457962031e6d652313ceebeefab19c
deadpearl <adilbayevameruert@gmail.com>
2024-06-10 02:17:10 +0500

    order

--- lib/app/presentation/ui/orders/orders_page.dart
+++ lib/app/presentation/ui/orders/orders_page.dart
@@ -34,7 +34,7 @@ class _OrdersPage extends State<OrdersPage> {
 
     print('token $token');
     final response = await http.get(
-      Uri.parse('$baseUrl/orders'),
+      Uri.parse('$baseUrl/cart'),
       headers: {
         'Authorization': 'Bearer $token',
       },
@@ -60,22 +60,117 @@ class _OrdersPage extends State<OrdersPage> {
 
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.background,
-      body: ScrollConfiguration(
-        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
-        child: ListView(
-          padding: const EdgeInsets.only(left: 0, right: 0, top: AppSize.topMargin),
-          children: const [
-            Center(
-              child: HeaderText("Мои заказы"),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: AppColor.background,
+    body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.only(top: 30, bottom: 20),
+          child: Text(
+            'Мои заказы',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
             ),
-            EmptyContentWidget()
-          ],
-        )
-      ),
-    );
-  }
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 16.0),
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: AppColor.background,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 4,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Список заказов',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              _isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : cart == null
+                      ? Center(child: Text('No data'))
+                      : ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: 1,
+                          itemBuilder: (BuildContext context, int index) {
+                            var order = cart['productDtos'][index];
+                            return Container(
+                              margin: EdgeInsets.only(bottom: 16.0),
+                              padding: EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 1,
+                                    blurRadius: 2,
+                                    offset: Offset(0, 1),
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Заказ #1',
+                                    style: TextStyle(
+                                      fontSize: 24.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Text('Статус: В ожидании',style: TextStyle(
+                                      fontSize: 17.0,
+                                    ),),
+                                  Text('Статус оплаты: оплачено',style: TextStyle(
+                                      fontSize: 17.0,
+                                    ),),
+                                  Text('День заказа: 10.06.2024 01:22',style: TextStyle(
+                                      fontSize: 17.0,
+                                    ),),
+                                  Text('День доставки: 12.06.2024',style: TextStyle(
+                                      fontSize: 17.0,
+                                    ),),
+                                  SizedBox(height: 8.0),
+                                  Text(
+                                    'Сумма заказа: 3250.00',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 156, 255, 7), // Other color you prefer
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
