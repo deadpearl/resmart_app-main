a1cd6b4181cd50daf4fdec3021a3e6944f0f0d0c
deadpearl <adilbayevameruert@gmail.com>
2024-06-10 01:06:51 +0500

    cart get

--- lib/app/presentation/ui/cart/cart_page.dart
+++ lib/app/presentation/ui/cart/cart_page.dart
@@ -65,166 +65,168 @@ class _CartPage extends State<CartPage> {
     }
   }
 
-
+double calculateTotalPrice() {
+  if (cart != null && cart['productDtos'] != null) {
+    double totalPrice = 0.0;
+    for (var productDto in cart['productDtos']) {
+      totalPrice += productDto['totalPrice'];
+    }
+    return totalPrice;
+  } else {
+    return 0.0;
+  }
+}
  
   void _order() {
     NavigationUtils.push(context, const PickupPage());
   }
 
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: AppColor.background,
-      body: ScrollConfiguration(
-          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: AppSize.topMargin),
-                child: Stack(
-                  children: [
-                    const Center(
-                      child: HeaderText("Корзина"),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(right: 16, top: 6),
-                      child: Row(
-                        children: [
-                          Expanded(child: Container()),
-                          InkWell(
-                            onTap: () {
-                              _order();
-                            },
-                            borderRadius: BorderRadius.circular(10),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                              child: const Text(
-                                'Заказать',
-                                style: TextStyle(
-                                    color: AppColor.primary,
-                                    fontFamily: 'Rubik',
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: AppColor.background,
+    body: ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: AppSize.topMargin),
+            child: Stack(
+              children: [
+                const Center(
+                  child: HeaderText("Корзина"),
                 ),
+                Container(
+            margin: const EdgeInsets.only(right: AppSize.horizontal, left: AppSize.horizontal, top: 50),
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              'Список продуктов',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
-              _generateInCart(size),
-              Container(
-                margin: const EdgeInsets.only(left: AppSize.horizontal, right: AppSize.horizontal, bottom: 20),
-                child: PrimaryButton("Заказать", () { _order(); }),
-              )
-            ],
-          )
-      ),
-    );
-  }
-
-  Widget _generateInCart(Size size) {
-    return Column(
-      children: [
-        _generateShopInCart(size, Shop(), [Product(), Product(), Product()]),
-        _generateShopInCart(size, Shop(), [Product(), Product(), Product()]),
-      ],
-    );
-  }
-
-  Widget _generateShopInCart(Size size, Shop shop, List<Product> products) {
-    return Column(
-      children: [
-        Container(
-          margin: const EdgeInsets.only(top: 14),
-          padding: const EdgeInsets.only(left: AppSize.horizontal, right: 18),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    HeaderText(shop.name, size: 18),
-                    Container(
-                      width: size.width / 2,
-                      color: AppColor.primary,
-                      height: 1,
-                      margin: const EdgeInsets.only(top: 12),
-                    )
-                  ],
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  NavigationUtils.push(context, ShopProductsPage(shop: shop));
-                },
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            ),
+          ),
+                Container(
+                  margin: const EdgeInsets.only(right: 16, top: 6),
                   child: Row(
                     children: [
-                      const Text(
-                        "Магазин",
-                        style: TextStyle(
-                            color: AppColor.primary,
-                            fontSize: 14,
-                            fontWeight: FontWeight.normal,
-                            fontFamily: 'Rubik'
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(left: 6),
-                        child: const Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: AppColor.primary,
-                          size: 18,
+                      Expanded(child: Container()),
+                      InkWell(
+                        onTap: () {
+                          _order();
+                        },
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                          child: const Text(
+                            'Заказать',
+                            style: TextStyle(
+                                color: AppColor.primary,
+                                fontFamily: 'Rubik',
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500
+                            ),
+                          ),
                         ),
                       )
                     ],
                   ),
                 ),
-              )
-            ],
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.only(top: 16, bottom: 14),
-          height: 258,
-          child: ScrollConfiguration(
-            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
-            child: ListView(
-              clipBehavior: Clip.none,
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: AppSize.horizontal, right: AppSize.horizontal - 14),
-              children: _generateInShopProducts(size, products),
+              ],
             ),
+          ),
+          Container(
+  margin: const EdgeInsets.only(left: AppSize.horizontal, right: AppSize.horizontal, bottom: 20),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: _isLoading
+        ? [Center(child: CircularProgressIndicator())] // Отображение индикатора загрузки, пока данные загружаются
+        : cart != null && cart['productDtos'] != null
+            ? cart['productDtos'].map<Widget>((productDto) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  
+                  child: Column(
+                  
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                       
+                      SizedBox(
+                        height: 100, // Фиксированная высота изображения
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            productDto['orderItems'][0]['product']['imageUrl'],
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        productDto['orderItems'][0]['product']['name'],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Количество: ${productDto['orderItems'][0]['quantity']}',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Цена: ${productDto['orderItems'][0]['price']}',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Общая цена: ${productDto['totalPrice']}',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList()
+            : [Text('Нет товаров в корзине')],
+  ),
+),
+ Container(
+            margin: const EdgeInsets.symmetric(horizontal: AppSize.horizontal, vertical: 10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Общая цена всех товаров: ${calculateTotalPrice()}',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: AppSize.horizontal, right: AppSize.horizontal, bottom: 20),
+            child: PrimaryButton("Заказать", () { _order(); }),
           )
-        )
-      ],
-    );
-  }
-
-  List<Widget> _generateInShopProducts(Size size, List<Product> products) {
-    var itemWidth = (size.width - AppSize.horizontal * 2 - 14) / 2;
-    List<Widget> productWidgets = [];
-    for (int i = 0; i < products.length; i++) {
-      productWidgets.add(
-        Container(
-          width: itemWidth,
-          margin: const EdgeInsets.only(right: 14),
-          child: ProductListItem(products[i], onPressed: () {
-            NavigationUtils.push(context, ProductPage(product: products[i]));
-          }),
-        )
-      );
-    }
-    return productWidgets;
-  }
-
+        ],
+      )
+    ),
+  );
+}
 }
